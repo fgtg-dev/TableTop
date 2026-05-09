55d8172e389551c04311f5003892e7dd9b51e329
fgtg.dev <fgtg.dev@gmail.com>
2026-05-09 09:05:13 +0800

    [feature1] Removal of unused assets and css and development of working POC

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
+const DIRECTION = {
+  NORTH: 0,
+  SOUTH: 180,
+  EAST: 90,
+  WEST: 270,
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const x = 3;
+  const y = 4;
+  const rotateDirection: 'North' | 'South' | 'East' | 'West' = 'South';
+  const direction = rotateDirection.toLocaleLowerCase();
+  let degrees = 0;
+  const maxXCoordinates = 5;
+  const maxYCoordinates = 5;
+
+  if(direction === 'North'.toLocaleLowerCase()) {
+    degrees = DIRECTION.NORTH;
+  } else if(direction === 'East'.toLocaleLowerCase()) {
+    degrees = DIRECTION.EAST;
+  } else if(direction === 'South'.toLocaleLowerCase()) {
+    degrees = DIRECTION.SOUTH;
+  } else if(direction === 'West'.toLocaleLowerCase()) {
+    degrees = DIRECTION.WEST;
+  }
+
+  const markerStyle = {
+    transform: `rotate(${degrees}deg)`,
+  };
 
   return (
     <>
       <section id="center">
         Table Top
-
+        <tbody>
+           {Array(maxYCoordinates).fill(1).map((el, iY) =>
+              <tr>
+                {Array(maxXCoordinates).fill(1).map((elX, iX) =>
+                  <td>
+                    { (y == iY && x == iX)  ? 
+                      <div style={markerStyle}>^</div> : <span>X</span> }
+                  </td>
+                )}
+              </tr>
+            )}
+        </tbody>
       </section>
- 
     </>
   )
 }
